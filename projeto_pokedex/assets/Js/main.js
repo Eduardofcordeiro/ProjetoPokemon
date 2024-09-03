a04d5fd75491c1f2e9c6ed0f41bba19690349a84
Eduardofcordeiro <nuk3.kz@gmail.com>
2024-09-03 18:59:34 -0300

    Add files via upload

--- projeto_pokedex/assets/Js/main.js
+++ projeto_pokedex/assets/Js/main.js
@@ -13,101 +13,104 @@ const popupMenu = document.getElementById('popupMenu');
     estado = 0;
     filtragemPokemon()
     
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map((pokemon) => `
-        <button class = "pop-upButton"> 
+    
+    //função que traz o carregamento de pokemons
+    function loadPokemonItens(offset, limit) {
+        pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+            const newHtml = pokemons.map((pokemon) => `
+            <button class = "pop-upButton"> 
             <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
-
-                    <img src="${pokemon.photo}" alt="${pokemon.name}">
-                </div>   
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+            <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>   
             </li>
-        `).join('');
-        
-        pokemonList.innerHTML += newHtml;
-    });
-}
-
-function filtragemPokemon() {
-    // Limpa a lista atual antes de aplicar os novos filtros
-    pokemonList.innerHTML = '';
-
-    switch (true){
-
-        case estado === 0:
-            maxRecords = 700;
-                limit = 25;
-                offset = 0; 
-                break;
-
-        case estado === 1:
-            maxRecords = 151;
-            limit = 25;
-            offset = 0;
-                break;
-                
-        case estado === 2:
-            maxRecords = 251;
-            limit = 25;
-            offset = 151;
-                break;
-        
-        
-        case estado === 3:
-        maxRecords = 386;
-        limit = 25;
-        offset = 251;
-                break;
-
-        case estado === 4:
-            maxRecords = 493;
-            limit = 25;
-            offset = 386;
-                break;
-
-        case estado === 5:
-            maxRecords = 649;
-            limit = 25;
-            offset = 493;
-                break;
-
-        case estado === 6:
-            maxRecords = 721;
-            limit = 25;
-            offset = 649;
-                break;
-
-        case estado === 7:
-            maxRecords = 809;
-            limit = 25;
-            offset = 721;
-                break;
-
-        case estado === 8:
-            maxRecords = 905;
-            limit = 25;
-            offset = 809;
-                break;
-        
-        case estado === 9:
-            maxRecords = 1008;
-            limit = 25;
-            offset = 905;
-                break;
+            `).join('');
+            
+            pokemonList.innerHTML += newHtml;
+        });
     }
 
-    loadPokemonItens(offset, limit);
-}
-
-// Evento de carregamento de mais pokemons
-loadMoreButton.addEventListener("click", () => {
+    // Função que filtra os pokemons que aparecerão
+    function filtragemPokemon() {
+        // Limpa a lista atual antes de aplicar os novos filtros
+        pokemonList.innerHTML = '';
+    
+        switch (true){
+    
+            case estado === 0:
+                maxRecords = 1008;
+                    limit = 50;
+                    offset = 0; 
+                    break;
+    
+            case estado === 1:
+                maxRecords = 151;
+                limit = 50;
+                offset = 0;
+                    break;
+                    
+            case estado === 2:
+                maxRecords = 251;
+                limit = 50;
+                offset = 151;
+                    break;
+            
+            
+            case estado === 3:
+            maxRecords = 386;
+            limit = 50;
+            offset = 251;
+                    break;
+    
+            case estado === 4:
+                maxRecords = 493;
+                limit = 50;
+                offset = 386;
+                    break;
+    
+            case estado === 5:
+                maxRecords = 649;
+                limit = 50;
+                offset = 493;
+                    break;
+    
+            case estado === 6:
+                maxRecords = 721;
+                limit = 50;
+                offset = 649;
+                    break;
+    
+            case estado === 7:
+                maxRecords = 809;
+                limit = 50;
+                offset = 721;
+                    break;
+    
+            case estado === 8:
+                maxRecords = 905;
+                limit = 50;
+                offset = 809;
+                    break;
+            
+            case estado === 9:
+                maxRecords = 1008;
+                limit = 50;
+                offset = 905;
+                    break;
+        }
+    
+        loadPokemonItens(offset, limit);
+    }
+    
+    // Evento de carregamento de mais pokemons
+    loadMoreButton.addEventListener("click", () => {
     offset += limit;
     const qtdRecordNextPage = offset + limit;
 
@@ -120,42 +123,54 @@ loadMoreButton.addEventListener("click", () => {
     }
 });
 
-// Evento do botão para alterar valores e executar a nova filtragem
+// botoes de filtragem
 GeracaoButton.addEventListener("click", () => {
-    estado = 1;  // Define o estado para 3 (Segunda Geração)
-    filtragemPokemon();  // Executa a nova filtragem com as alterações
+    estado = 0;  
+    filtragemPokemon();
+});
+GeracaoButton1.addEventListener("click", () => {
+    estado = 1;
+    filtragemPokemon();
 });
 GeracaoButton2.addEventListener("click", () => {
-    estado = 2;  // Define o estado para 3 (Segunda Geração)
-    filtragemPokemon();  // Executa a nova filtragem com as alterações
+    estado = 2;
+    filtragemPokemon();
+
 });
 GeracaoButton3.addEventListener("click", () => {
-    estado = 3;  // Define o estado para 3 (Segunda Geração)
-    filtragemPokemon();  // Executa a nova filtragem com as alterações
+    estado = 3;
+    filtragemPokemon();
+
 });
 GeracaoButton4.addEventListener("click", () => {
-    estado = 4;  // Define o estado para 3 (Segunda Geração)
-    filtragemPokemon();  // Executa a nova filtragem com as alterações
+    estado = 4;
+    filtragemPokemon();
+
 });
 GeracaoButton5.addEventListener("click", () => {
-    estado = 5;  // Define o estado para 3 (Segunda Geração)
-    filtragemPokemon();  // Executa a nova filtragem com as alterações
+    estado = 5;
+    filtragemPokemon();
+
 });
 GeracaoButton6.addEventListener("click", () => {
-    estado = 6;  // Define o estado para 3 (Segunda Geração)
-    filtragemPokemon();  // Executa a nova filtragem com as alterações
+    estado = 6;
+    filtragemPokemon();
+
 });
 GeracaoButton7.addEventListener("click", () => {
-    estado = 7;  // Define o estado para 3 (Segunda Geração)
-    filtragemPokemon();  // Executa a nova filtragem com as alterações
+    estado = 7;
+    filtragemPokemon();
+
 });
 GeracaoButton8.addEventListener("click", () => {
-    estado = 8;  // Define o estado para 3 (Segunda Geração)
-    filtragemPokemon();  // Executa a nova filtragem com as alterações
+    estado = 8;
+    filtragemPokemon();
+
 });
 GeracaoButton9.addEventListener("click", () => {
-    estado = 9;  // Define o estado para 3 (Segunda Geração)
-    filtragemPokemon();  // Executa a nova filtragem com as alterações
+    estado = 9;
+    filtragemPokemon();
+
     
 });
 
